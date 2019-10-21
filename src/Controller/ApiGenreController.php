c0d37a1e00962a113a832de0512db0875c376880
Krikod <kristen.kod@gmail.com>
2019-10-21 17:10:28 +0200

    api for create genre

--- src/Controller/ApiGenreController.php
+++ src/Controller/ApiGenreController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Genre;
 use App\Repository\GenreRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiGenreController extends AbstractController
@@ -43,5 +46,50 @@ class ApiGenreController extends AbstractController
         );
 //        return new JsonResponse($resultat, 200, [], true);
         return new JsonResponse($resultat, Response::HTTP_OK, [], true);
+    }
+
+    /**
+     * @Route("/api/genres",
+     *     name="api_genres_create",
+     *     methods={"POST"})
+     */
+    public function create(Request $request,
+                           ObjectManager $manager,
+                           SerializerInterface $serializer)
+    {
+        $data = $request->getContent();
+//        $genre = new Genre();
+//        $serializer->deserialize(
+//          $data,
+//          Genre::class,
+//          'json',
+//          ['object_to_populate' => $genre]);
+
+//        Solution du dessus, ou bien:
+        $genre = $serializer->deserialize(
+            $data,
+            Genre::class,
+            'json');
+
+        $manager->persist($genre);
+        $manager->flush();
+
+// Pour une création pas de résultat donné ici (null, ou message).
+// Dans [] = ce qu'on rajoute dans le header.
+//        return new JsonResponse(
+//            "Le genre a bien été créé.",
+//            Response::HTTP_CREATED,
+//            ["location" => "api/genres/" . $genre->getId()],
+//            true);
+
+//        Ou, pour renvoyer l'url absolue:
+            return new JsonResponse(
+                "Le genre a bien été créé.",
+                Response::HTTP_CREATED,
+            ["location" => $this->generateUrl(
+                'api_genres_show',
+                ["id" => $genre->getId()],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            )], true);
     }
 }
